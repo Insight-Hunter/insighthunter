8c4c1cda6c76b6324609898717e62b69e515141f
jameshunterturner <jameshunterturner@gmail.com>
2026-02-25 22:10:25 +0000

    :pre-deploy git

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,10 @@ git push
 export CLOUDFLARE_ACCOUNT_ID="18c8e61a3669253dcfd0c7eec6be36a3"
 export CLOUDFLARE_API_TOKEN="kAY9u88TaeuI9wByQkismZ2oGjBWqf5mVBhDTYNE"
 
+wrangler d1 execute insighthunter-auth --file=apps/insighthunter-auth/schema.sql -c apps/insighthunter-auth/wrangler.toml --remote
+# Make sure your CLOUDFLARE_ACCOUNT_ID and CLOUDFLARE_API_TOKEN are set
+wrangler d1 execute insight-hunter --file=apps/insighthunter-auth/schema.sql -c apps/insighthunter-auth/wrangler.toml --remote
+
 wrangler deploy /home/user/insighthunter/apps/insighthunter-auth
 ## wrangler deploy /home/user/insighthunter/apps/insighthunter-main
 
