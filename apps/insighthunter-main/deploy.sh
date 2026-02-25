02bd19bf46d7e561f6db50015422a9ff631eb779
jameshunterturner <jameshunterturner@gmail.com>
2026-02-25 11:02:42 +0000

    :pre-deploy git

--- apps/insighthunter-main/deploy.sh
+++ apps/insighthunter-main/deploy.sh
@@ -7,7 +7,7 @@ git push
 export CLOUDFLARE_ACCOUNT_ID="18c8e61a3669253dcfd0c7eec6be36a3"
 export CLOUDFLARE_API_TOKEN="kAY9u88TaeuI9wByQkismZ2oGjBWqf5mVBhDTYNE"
 
-wrangler pages deploy /apps/insighthunter-main 
+wrangler pages deploy .
 
 
 echo " "
