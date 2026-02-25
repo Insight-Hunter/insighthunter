9bbc4e6b15d0ee8fc40713ff59828de142cc7e98
jameshunterturner <jameshunterturner@gmail.com>
2026-02-25 00:16:51 +0000

    auth updates, subdomain additions

--- apps/insighthunter-lite/.wrangler/tmp/dev-fsPRyI/index.js
+++ apps/insighthunter-lite/.wrangler/tmp/dev-fsPRyI/index.js
@@ -32,7 +32,7 @@ var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__ge
   mod
 ));
 
-// .wrangler/tmp/bundle-4bEefO/checked-fetch.js
+// .wrangler/tmp/bundle-VUGpiV/checked-fetch.js
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
     (typeof request === "string" ? new Request(request, init) : request).url
@@ -50,7 +50,7 @@ function checkURL(request, init) {
 }
 var urls;
 var init_checked_fetch = __esm({
-  ".wrangler/tmp/bundle-4bEefO/checked-fetch.js"() {
+  ".wrangler/tmp/bundle-VUGpiV/checked-fetch.js"() {
     "use strict";
     urls = /* @__PURE__ */ new Set();
     __name(checkURL, "checkURL");
@@ -527,11 +527,11 @@ var require_papaparse_min = __commonJS({
   }
 });
 
-// .wrangler/tmp/bundle-4bEefO/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-VUGpiV/middleware-loader.entry.ts
 init_checked_fetch();
 init_modules_watch_stub();
 
-// .wrangler/tmp/bundle-4bEefO/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-VUGpiV/middleware-insertion-facade.js
 init_checked_fetch();
 init_modules_watch_stub();
 
@@ -4896,7 +4896,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-4bEefO/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-VUGpiV/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -4930,7 +4930,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-4bEefO/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-VUGpiV/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
