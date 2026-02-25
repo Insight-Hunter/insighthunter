10024a8fb06dcc9a096784fca76df5572188308e
jameshunterturner <jameshunterturner@gmail.com>
2026-02-25 12:31:24 +0000

    :pre-deploy git

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ export CLOUDFLARE_ACCOUNT_ID="18c8e61a3669253dcfd0c7eec6be36a3"
 export CLOUDFLARE_API_TOKEN="kAY9u88TaeuI9wByQkismZ2oGjBWqf5mVBhDTYNE"
 
 wrangler pages deploy /home/user/insighthunter/apps/insighthunter-main/public
-
+wrangler deploy /home/user/insighthunter/apps/insighthunter-main
 
 echo " "
 echo "Secrets set and deployment command ran"
