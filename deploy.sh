ac4b07a4ffe93abdacf290bdd9012dd007214c94
jameshunterturner <jameshunterturner@gmail.com>
2026-02-27 08:54:03 +0000

    :pre-deploy git

--- deploy.sh
+++ deploy.sh
@@ -15,22 +15,17 @@ export CLOUDFLARE_API_TOKEN="kAY9u88TaeuI9wByQkismZ2oGjBWqf5mVBhDTYNE"
 ##wrangler secret put JWT_SECRET="5afde2d9ba897193d88ba038ed3edd03870ccae6338077cec5c50e333c9de777"
 #wrangler secret put STRIPE_PUBLISHABLE_KEY
 
-cd apps/insighthunter-auth
+
 #npx wrangler d1 execute insight-users --remote --file=./migrations/0001_initial_schema/0001_initial_schema.sql
-npx wrangler deploy --name insighthunter-auth
-cd .. 
-cd insighthunter-main
-npx wrangler deploy --name insighthunter-main
-cd ..
-cd insighthunter-bookkeeping
-npx wrangler deploy --name insighthunter-bookkeeping
-#cd .. /insighthunter-payments
-#npx wrangler deploy --name insighthunter-payments
-#cd .. /insighthunter-webhooks
+npx wrangler deploy apps/insighthunter-auth --name insighthunter-auth
+
+npx wrangler deploy apps/insighthunter-main --name insighthunter-main
+
+npx wrangler deploy apps/insighthunter-bookkeeping --name insighthunter-bookkeeping
+#cd .. /isighthunter-webhookss
 #npx wrangler deploy --name insighthunter-webhooks
-cd .. 
-cd insighthunter-pbx 
-npx wrangler deploy --name insighthunter-pbx
+
+npx wrangler deploy apps/insighthunter-pbx --name insighthunter-pbx
 
 ##wrangler d1 execute insighthunter-auth --file=apps/insighthunter-auth/schema.sql -c apps/insighthunter-auth/wrangler.toml --remote
 # Make sure your CLOUDFLARE_ACCOUNT_ID and CLOUDFLARE_API_TOKEN are set
