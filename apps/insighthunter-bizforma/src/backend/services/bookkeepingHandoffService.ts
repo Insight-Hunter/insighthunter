a64a2c76472a31a5a1801a4692d08387070f4156
James Michael Hunter Turner <jmichaelht@gmail.com>
2026-04-13 17:16:11 -0400

    Delete apps/insighthunter-bizforma/src/backend directory

--- apps/insighthunter-bizforma/src/backend/services/bookkeepingHandoffService.ts
+++
@@ -1,7 +0,0 @@
-import type { Env, FormationCase } from '../types';
-
-export const bookkeepingHandoffService = {
-  async onFormationComplete(env: Env, formation: FormationCase) {
-    console.log('Formation complete; seeding CoA', formation.id);
-  },
-};
