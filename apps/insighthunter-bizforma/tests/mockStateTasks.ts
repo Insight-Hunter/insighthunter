b93d786151968f096b82cda51e1e7da6c273e755
James Michael Hunter Turner <jmichaelht@gmail.com>
2026-04-13 20:22:42 -0400

    Create mockStateTasks.ts

---
+++ apps/insighthunter-bizforma/tests/mockStateTasks.ts
@@ -0,0 +1,12 @@
+export const mockStateTasks = [
+  {
+    id: "formation-filing",
+    title: "File LLC formation documents",
+    feeCents: 10000,
+  },
+  {
+    id: "annual-report",
+    title: "Track annual report or registration",
+    feeCents: 5000,
+  },
+];
