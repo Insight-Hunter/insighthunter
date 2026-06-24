f59d274414ac14ed10121789181c267c501db4e1
James Michael Hunter Turner <jmichaelht@gmail.com>
2026-06-24 00:40:46 -0400

    Update main.tsx

--- apps/insighthunter-bizforma/src/main.tsx
+++ apps/insighthunter-bizforma/src/main.tsx
@@ -1,13 +1,11 @@
-// src/main.tsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from '../App';
+import '../styles/globals.css';
 
-const rootEl = document.getElementById("root");
-if (!rootEl) throw new Error("Root element not found");
-
-ReactDOM.createRoot(rootEl).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
+
